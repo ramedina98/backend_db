0ab5120a4e7561d713f4cd86ed659bfd56dbe53b
ramedina98 <rmedinamartindelcampo@gmail.com>
2024-07-06 00:02:35 -0600

    update connection

--- app/models/Connection_db.php
+++ app/models/Connection_db.php
@@ -1,14 +1,37 @@
 <?php
-    // cargamos las variables de entorno...
-    $dotenv = parse_ini_file('.env');
+    /**
+     * @author Ricardo Medina
+     * 05/07/2024
+     *
+     * This is the connection to the DB, where ETL deposits all the data extracted from MongoDB...
+     */
+
+    //we use autload.php to be able to call our environment variables...
+    require __DIR__ . '/../../vendor/autoload.php';
+    use Dotenv\Dotenv;
+
+    //try catch for error handling....
+    try {
+        $dotenv = Dotenv::createImmutable(dirname(__DIR__, 2));
+        $dotenv->load();
+    } catch (Exception $e) {
+        die("Error loading Dotenv: " . $e->getMessage());
+    }
 
     class Connection {
         // TODO: Llamar a las variables de entorno...
-        private $host = $dotenv['DB_HOST'];
-        private $dbname = $dotenv['DB_DATABASE'];
-        private $username = $dotenv['DB_USERNAME'];
-        private $password = $dotenv['DB_PASSWORD'];
+        private $host;
+        private $dbname;
+        private $username;
+        private $password;
         private $conn;
+
+        public function __construct(){
+            $this->host = $_ENV['DB_HOST'];
+            $this->dbname = $_ENV['DB_DATABASE'];
+            $this->username = $_ENV['DB_USERNAME'];
+            $this->password = $_ENV['DB_PASSWORD'];
+        }
 
         // TODO: Descomentar cuando la base de datos este creada y tenga acceso a todo...
         /*public function getConnection(){
