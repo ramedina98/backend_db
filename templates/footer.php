6fe193a84df0ab14c9a2b67cbfe3cc22c903b516
ramedina98 <rmedinamartindelcampo@gmail.com>
2024-07-13 10:08:40 -0600

    add: the footer is ready

--- templates/footer.php
+++ templates/footer.php
@@ -1,7 +1,18 @@
-    <!--TODO: Se debe de crear un diseño para esto: buscar idas de bootstrap-->
-    <footer style="border: 1px solid green; width: 100%;">
-        Este es el footer
+    <footer class="navbar">
+        <div id="copy_brand_container" class="container-fluid">
+            <a id="a_brand" class="navbar-brand" href="#">
+                <img src="https://cdn-icons-png.flaticon.com/512/3161/3161158.png" alt="Logo" width="80" height="74" class="d-inline-block align-text-top">
+                Ventas DB
+            </a>
+            <div id="copy_conteiner">
+                <span>
+                    © 2024-07-10 Copyright Backend Team
+                </span>
+            </div>
+        </div>
     </footer>
+
+    <!--This is our script-->
     <script src="<?php echo BASE_URL; ?>public/js/script.js"></script>
     <!--cdn de boostrap, scripts-->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
