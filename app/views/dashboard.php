eaf41b706eec86b292968100043b3b1b4ae0b181
ramedina98 <rmedinamartindelcampo@gmail.com>
2024-07-13 09:33:15 -0600

    add: header's done

--- app/views/dashboard.php
+++ app/views/dashboard.php
@@ -4,29 +4,32 @@
      */
     include './templates/header.php';
 ?>
-<!--TODO: aqui es donde iria el contenido de la pagina, las graficas y demás-->
-<section style="width: 100%; height: 100vh; border: 1px solid blue">
-<?php
-/**
- * TODO: AQUI SE DEBE AGREGAR OTRA GRAFICA...
- * NOTE: Esto tambien es un ejemplo
- * if (!empty($data_dm)) {
- * echo '<h2>Productos:</h2>';
- * echo '<pre>';
- *   print_r($data_dm);
-*  echo '</pre>';
-*} else {
-*    echo 'No se encontraron productos.';
-*}
- */
-?>
-</section>
+<!--TODO: acomodar esta parte-->
+<main>
+    <!--TODO: aqui es donde iria el contenido de la pagina, las graficas y demás-->
+    <section style="width: 100%; height: 100vh;">
+    <?php
+    /**
+     * TODO: AQUI SE DEBE AGREGAR OTRA GRAFICA...
+     * NOTE: Esto tambien es un ejemplo
+     * if (!empty($data_dm)) {
+     * echo '<h2>Productos:</h2>';
+     * echo '<pre>';
+     *   print_r($data_dm);
+    *  echo '</pre>';
+    *} else {
+    *    echo 'No se encontraron productos.';
+    *}
+    */
+    ?>
+    </section>
 
-<section style="width: 100%; height: 100vh; border: 1px solid blue">
-<?php
- //TODO: Aquí se debe agregar una grafica...
-?>
-</section>
+    <section style="width: 100%; height: 100vh;">
+    <?php
+    //TODO: Aquí se debe agregar una grafica...
+    ?>
+    </section>
+</main>
 <?php
     /**
      * Se esta agregando de manera dinamica el footer...
--- templates/header.php
+++ templates/header.php
@@ -1,17 +1,53 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <!--TODO: poner el SEO-->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>UNEDL Products Dashboard</title>
     <link rel="stylesheet" href="<?php echo BASE_URL; ?>public/css/style.css">
     <link rel="shortcut icon" href="<?php echo BASE_URL; ?>public/icons/favicon.ico" type="image/x-icon">
-    <!--Font-->
     <!--cdn de bootstrap, stilos-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
-<!--TODO: Buscar un diseño padre para el header en boostrap-->
-<header style="color: red; width: 100%; border:1px solid red;">
-    Aqui iria el header
+<header>
+    <nav class="navbar fixed-top">
+        <div class="container-fluid">
+            <a class="a_color_nav_label" href="#">Dashboard Ventas BD</a>
+            <button id="btn_navbar_toggler" class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
+                <span id="icon_navbar_toggle" class="navbar-toggler-icon"></span>
+            </button>
+            <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
+                <div class="offcanvas-header">
+                    <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">Base de Datos II</h5>
+                    <button type="button" class="btn-close btn-close-dark" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                </div>
+                <div class="offcanvas-body">
+                    <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Dashboard</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Probedores</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Tiendas
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#">Unedl 90</a></li>
+                                <li><a class="dropdown-item" href="#">Centro Universitario</a></li>
+                                <li><a class="dropdown-item" href="#">Casa unedl</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <form class="d-flex mt-3" role="search">
+                        <input id="input_search" class="form-control me-2" type="search" placeholder="Busqueda de clientes leales" aria-label="Search">
+                        <button id="btn_search" class="btn">Buscar</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </nav>
 </header>
