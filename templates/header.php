bbb5cbe624dd0cb7030ac191b734be41f7e104d6
ramedina98 <rmedinamartindelcampo@gmail.com>
2024-08-02 19:58:37 -0600

    fix: header template

--- templates/header.php
+++ templates/header.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <!--TODO: poner el SEO-->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>UNEDL Products Dashboard</title>
